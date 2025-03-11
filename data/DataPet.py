b37126e48eaa4f415059df7c3a9a8ee68b85e97f
Jhoan Sebastian Duque Zapata <38737376+sebastian946@users.noreply.github.com>
2025-03-11 06:56:01 -0500

    firsh changes

---
+++ data/DataPet.py
@@ -0,0 +1,26 @@
+from faker import Faker
+
+class dataPet:
+    def __init__(self):
+        pass
+
+    def genData():
+
+        json = {
+  "id": "10",
+  "name": "doggie",
+  "category": {
+    "id": 1,
+    "name": "Dogs"
+  },
+  "photoUrls": [
+    "string"
+  ],
+  "tags": [
+    {
+      "id": 0,
+      "name": "12f"
+    }
+  ],
+  "status": "available"
+}
